e94f5b19282e12ee7b30d4070c5567f00af18628
the_dump <thedump58246@gmail.com>
2022-03-19 18:53:24 +0300

    pc стоцеанарны

--- js/7222.js
+++ js/7222.js
@@ -1,14 +1,14 @@
-let red = 0
-let green = 0
-let blue = 0
-setInterval(() => {
-    document.querySelector('html').style.background = `rgb(${red}, ${green}, ${blue})`
-    if (red <= 255)  {
- red++ ,
-    }
-    else {
-red--,
-    }
-    green++
-    blue--
-})
+// let red = 0
+// let green = 0
+// let blue = 0
+// setInterval(() => {
+//     document.querySelector('html').style.background = `rgb(${red}, ${green}, ${blue})`
+//     if (red <= 255)  {
+//  red++ ,
+//     }
+//     else {
+// red--,
+//     }
+//     green++
+//     blue--
+// })
